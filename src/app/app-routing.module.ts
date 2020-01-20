7ca09e9c0180e92522ac53a8b321b911a815e99d
unknown <Stagiaire@LOL157.edu-lol.loc>
2020-01-20 10:35:41 +0100

    ajout des commentaires

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router'
 import { RecetteComponent } from './components/recette/recette.component'
 import { RecettesComponent } from './components/recettes/recettes.component'
 import { RecetteRechercheComponent } from './components/recette-recherche/recette-recherche.component'
+import { UserProfileComponent } from './components/user-profile/user-profile.component'
 
 
 const routes: Routes = [
   {path: 'recettes', component: RecettesComponent},
   {path: 'recettes/:id', component: RecetteComponent},
   {path: 'recettes-recherche', component: RecetteRechercheComponent}
+  {path: 'user/:id', component: UserProfileComponent},
 ]
 
 @NgModule({
